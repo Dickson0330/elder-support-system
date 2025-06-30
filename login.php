c5396fb9f28e6e97809482c6aecc1a5f3dbbee01
Thomas233-x <thomasliu129@gmail.com>
2025-06-30 10:03:30 +0900

    login

---
+++ login.php
@@ -0,0 +1,5 @@
+<form method="post" action="login.php">
+  ユーザー名: <input type="text" name="username"><br>
+  パスワード: <input type="password" name="password"><br>
+  <input type="submit" value="ログイン">
+</form>
